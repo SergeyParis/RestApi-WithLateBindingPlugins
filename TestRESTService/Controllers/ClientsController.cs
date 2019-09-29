9da82953ae465b4bdf6d2134ee67e01a402b2eea
sergiyparis <bt1bt1990@gmail.com>
2019-09-29 19:53:29 +0300

    Init repo

---
+++ TestRESTService/Controllers/ClientsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestRESTService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ILogger<ClientsController> _logger;
+
+        public ClientsController(ILogger<ClientsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            
+        }
+    }
+}
