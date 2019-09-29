667c314fe46c0c721436dc4280ee88723b0ac62c
sergiyparis <bt1bt1990@gmail.com>
2019-09-30 01:53:46 +0300

    Change build path

--- TestRESTService/Helpers/PluginsLoader.cs
+++ TestRESTService/Helpers/PluginsLoader.cs
@@ -1,24 +1,28 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using TestCommon.Contracts;
-using TestCommon.Domain;
 
 namespace TestRESTService.Helpers
 {
     public static class PluginsLoader
     {
-        public static void AddPlugins(this IServiceCollection serviceCollection, IEnumerable<PluginInfo> plugins)
+        private const string PathToArtifactsDebug = @"../artifacts/netcoreapp3.0/_debug/";
+        private const string PathToArtifactsRelease = @"../artifacts/netcoreapp3.0/";
+
+        private static string _currentPathToArtifacts = PathToArtifactsRelease;
+        
+        public static void AddPlugins(this IServiceCollection serviceCollection, IEnumerable<string> plugins)
         {
             foreach (var pluginPath in plugins)
             {
-                RegisterOneGetAssembly(serviceCollection, GetFullPath(pluginPath.ProjectName, pluginPath.RelativePath));
+                RegisterOneGetAssembly(serviceCollection, GetFullPath(pluginPath));
             }
         }
 
+        public static void UseDebug() => _currentPathToArtifacts = PathToArtifactsDebug;
+        
         private static void RegisterOneGetAssembly(IServiceCollection serviceCollection, string path)
         {
             var assembly = Assembly.LoadFrom(path);
@@ -39,7 +43,7 @@ namespace TestRESTService.Helpers
 
         private static string GenericName(this string name) => name.Split("`").First();
 
-        private static string GetFullPath(string projectName, string relativePath) =>
-            $".\\{relativePath}..\\{projectName}\\bin\\Debug\\netcoreapp3.0\\{projectName}.dll";
+        private static string GetFullPath(string projectName) =>
+            $"{_currentPathToArtifacts}{projectName}.dll";
     }
 }
--- TestRESTService/Startup.cs
+++ TestRESTService/Startup.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
-using Microsoft.Extensions.Options;
 using TestRESTService.Helpers;
 
 namespace TestRESTService
@@ -32,7 +23,7 @@ namespace TestRESTService
             
             services.AddPlugins(new []
             {
-                new PluginInfo { ProjectName = "ServicePlugin" } 
+                "ServicePlugin"
             });
         }
 
@@ -42,6 +33,7 @@ namespace TestRESTService
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                PluginsLoader.UseDebug();
             }
             
             app.UseHttpsRedirection();
